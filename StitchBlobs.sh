20d5c844e39fc7819c12a382cacffe6f853541f6
Jacob Fields <jacobfields@ou.edu>
2025-05-06 15:37:09 -0500

    Update StitchBlobs.sh

--- StitchBlobs.sh
+++ StitchBlobs.sh
@@ -1,10 +1,10 @@
-StitchBlobs --in_list "hgt.19502022_blocktag_files.txt" \
-            --out_list "hgt.19502022_blockid_files.txt" \
-            --var "Blocking" \
-            --latname "latitude" \
-            --lonname "longitude" \
-            --mintime "5d" \
-            --min_overlap_prev 50 \
+#StitchBlobs joins instantaneous blocks into continuous 'blobs' that persist through multiple time steps. 
+#These blobs represent unique blocking events, with each event having a unique ID.
 
-#            --in_list "hgt.19912020_blocktag_files.txt" \
-#           --out_list "hgt.19912020_blockid_files.txt" \
+StitchBlobs --in_list "blocktag_file.txt" \ #Input file (File created by IB_trimmed_overpole.py)
+            --out_list "blockid_file.txt" \ #Output file
+            --var "block_tag" \ #Variable in input file being called in (instantaneous blocks)
+            --latname "latitude" \ 
+            --lonname "longitude" \
+            --mintime "5d" \ #Threshold for the minimum duration of blocking events
+            --min_overlap_prev 50 \ #Threshold for percentage of blocking 'blob' that spatially overlaps between consecutive time steps
